9e7d3d4dcb53bf974719d0c41a0a9b99db8a7733
Vishal Poonacha <56363146+vishalpoonacha@users.noreply.github.com>
2023-01-20 15:07:16 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,20 @@
-node default {}
+node default {
+  file {'/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner => 'root',
+  }
+}
+
 node 'localhost.localdomain' {
   include role::master_server
+}
+
+
+node /^web/ {
+  include role::app_server  
+}
+
+node /^db/ {
+  include role::db_server  
 }
